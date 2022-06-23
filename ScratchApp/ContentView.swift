f5fdf8c96ca74fb813ceb0e87cc4a81b62c15e4a
Ahmad Zaky Wisnumurti <ahmad.zaky@ruangguru.com>
2022-06-23 19:04:04 +0700

    Initial Commit

---
+++ ScratchApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScratchApp
+//
+//  Created by Ahmad Zaky W on 23/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScratchApp/ScratchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchAppApp.swift
+//  ScratchApp
+//
+//  Created by Ahmad Zaky W on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ScratchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
